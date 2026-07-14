988ca61929290bc40ad937db0a9e2960277e298a
Nic Vega <vega.nm@gmail.com>
2026-07-14 00:27:15 -0400

    update 2025 phage

--- GlobiPhageHostRange_uPub.R
+++ GlobiPhageHostRange_uPub.R
@@ -2,37 +2,43 @@
 # Isolation host A. globiformis B-2979
 # Emory University phage 2023-2024
 
-pacman::p_load(tidyverse, ggpubr, patchwork, cowplot, GGally)
+pacman::p_load(tidyverse, ggpubr, patchwork, cowplot, GGally, readxl)
 xTextSize<-14
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # read in cleaned data
-HostRange24.all<-read.csv("GlobiHostRangeData2024.csv", header=TRUE)
+HostRange24.all<-read.csv("GlobiHostRangeData_2024_2025.csv", header=TRUE)
 #view(HostRange24.all)
+
+#unique(HostRange24.all$Bacteria)
+
+#GTF may not be unique - remove
+HostRange24.all <- HostRange24.all |>
+  dplyr::filter(Name != "GreenTreeFrog")
 
 # heatmap EOPs
 HostRange24.EOPhosts<-HostRange24.all |>
-  dplyr::filter(StrainID=="B2880" | StrainID=="B24025" | StrainID=="B24478" | StrainID=="B24479") |>
+  dplyr::filter(Bacteria=="B2880" | Bacteria=="B24025" | Bacteria=="B24478" | Bacteria=="B24479") |>
   mutate(logEOP=log10(EOP)) 
 
 # reorder for plotting
-HostRange24.EOPhosts$StrainID <-factor(HostRange24.EOPhosts$StrainID, 
+HostRange24.EOPhosts$Bacteria <-factor(HostRange24.EOPhosts$Bacteria, 
                                        levels=c('B2880', 'B24025', 'B24478', 'B24479'))
   
 # Plot heatmap of log10(EOP). NA (no plaque formation recorded) is in grey
-pEOPbyPhage.all.hm<-HostRange24.EOPhosts |>
-  ggplot(aes(y=Name, x=StrainID, fill=logEOP)) + 
+pEOPbyPhage.filtered.hm<-HostRange24.EOPhosts %>%
+  ggplot(aes(y=Name, x=Bacteria, fill=logEOP)) + 
   geom_tile() +
   scale_fill_gradient2(low="#edf8b1", mid="#7fcdbb", high="navy", midpoint=-3, na.value="grey50") +
   #scale_fill_distiller(palette = "RdPu", direction=-1, na.value="grey50") +
   theme_classic()+
   theme(text=element_text(size=xTextSize),
-        axis.text.x = element_text(angle=45, hjust=1),
+        axis.text.x = element_text(angle=90, hjust=1),
         plot.title = element_text(hjust=0.5))+
   labs(y="Phage", x="Bacteria", title="Plating Efficiency")+
-  facet_wrap(~Set)
-pEOPbyPhage.all.hm
+  facet_grid(cols=vars(Set))
+pEOPbyPhage.filtered.hm
 
 
 # filter only the entries with EOP values.
@@ -58,7 +64,7 @@ HostRange24.EOP.all %>%
   geom_point()+
   theme_bw()+
   geom_vline(xintercept=0, color="red")+
-  facet_wrap(~StrainID)
+  facet_wrap(~Bacteria)
 
 cols<-c("N"="black", "Y"="blue")
 # and without faceting
@@ -66,7 +72,7 @@ pEOPbyPhage.all<-HostRange24.EOP.all %>%
   ggplot(aes(y=logEOP, x=Name))+
   geom_boxplot()+
   #  geom_point(aes(pch=LFW, color=LFW), size=2)+
-  geom_jitter(aes(pch=StrainID, color=LFW), width=0.1, size=2)+
+  geom_jitter(aes(pch=Bacteria, color=LFW), width=0.1, size=2)+
   theme_bw()+
   scale_color_manual(values=cols)+
   scale_shape_manual(values=c(17, 16, 15, 2))+
@@ -81,10 +87,10 @@ pEOPbyPhage.all
 # and by host across all phages
 pEOPbyHost.all<-HostRange24.EOP.all %>%
   #dplyr::filter(Set == 1) %>%
-  ggplot(aes(y=logEOP, x=StrainID))+
+  ggplot(aes(y=logEOP, x=Bacteria))+
   geom_boxplot()+
   #geom_violin()+
-  geom_jitter(aes(pch=StrainID, color=LFW), width=0.05, size=2)+
+  geom_jitter(aes(pch=Bacteria, color=LFW), width=0.05, size=2)+
 #  geom_jitter(aes(color=LFW), width=0.05, size=2)+
   theme_bw()+
   scale_color_manual(values=cols)+
@@ -94,8 +100,8 @@ pEOPbyHost.all<-HostRange24.EOP.all %>%
         plot.title = element_text(hjust=0.5))+
   geom_hline(yintercept=0, color="red")+
   labs(y=expression(log[10](EOP)), pch="Bacteria", title="EOP by host", x="") +
-  facet_wrap(~HostSpecies, scales="free_x", nrow = 1)+
-  facet_wrap(~Set, ncol=1)
+  #facet_wrap(~HostSpecies, scales="free_x", nrow = 1)+
+  facet_grid(cols=vars(Set))
 pEOPbyHost.all
 
 
@@ -103,29 +109,35 @@ pEOPbyHost.all
 # How do EOPs compare on the two non-isolation-host globi strains?
 HostRange24.EOP.globi<-  HostRange24.EOP.all %>%
   dplyr::filter(LFW!="Y") %>%
-  dplyr::select(Set, PhageID, Name, StrainID, logEOP) %>%
-  dplyr::filter(StrainID=="B24025" | StrainID=="B2880" | StrainID=="B24478") %>%
-  pivot_wider(names_from=StrainID, values_from=logEOP)
+  dplyr::select(Set, PhageID, Name, Bacteria, logEOP) %>%
+  dplyr::filter(Bacteria=="B24025" | Bacteria=="B2880" | Bacteria=="B24478") %>%
+  pivot_wider(names_from=Bacteria, values_from=logEOP)
 
 
 ### stat tests
 
 # Wilcox
-wilcox.test(HostRange24.EOP.globi$B2880, HostRange24.EOP.globi$B24478) #p=0.1127
-wilcox.test(HostRange24.EOP.globi$B2880, HostRange24.EOP.globi$B24025) #p=0.005128 
-wilcox.test(HostRange24.EOP.globi$B24478, HostRange24.EOP.globi$B24025) #p=0.00658
+wilcox.test(HostRange24.EOP.globi$B2880, HostRange24.EOP.globi$B24478) #p=0.3576
+wilcox.test(HostRange24.EOP.globi$B2880, HostRange24.EOP.globi$B24025) #p=0.002453 
+wilcox.test(HostRange24.EOP.globi$B24478, HostRange24.EOP.globi$B24025) #p=0.02794
 
 # Linear regressions
-lm.2880.24478<-lm(B2880~B24478, data=HostRange24.EOP.globi)
+HostRange24.EOP.globi.filtered<-HostRange24.EOP.globi |>
+  dplyr::filter(B2880 >=-4 & B24478>=-4)
+lm.2880.24478<-lm(B2880~B24478, data=HostRange24.EOP.globi.filtered)
 summary(lm.2880.24478)
 # n.s.
 
-lm.2880.24025<-lm(B2880~B24025, data=HostRange24.EOP.globi)
+HostRange24.EOP.globi.filtered<-HostRange24.EOP.globi |>
+  dplyr::filter(B2880 >=-4 & B24025>=-4)
+lm.2880.24025<-lm(B2880~B24025, data=HostRange24.EOP.globi.filtered)
 summary(lm.2880.24025)
-#m = 0.8145, b = -1.6479, p=0.058 on F test
-#R2adj = 0.2069
+#m = 0.7004, b = -0.9731, p=0.000143 on F test
+#R2adj = 0.7572
 
-lm.24478.24025<-lm(B24478~B24025, data=HostRange24.EOP.globi)
+HostRange24.EOP.globi.filtered<-HostRange24.EOP.globi |>
+  dplyr::filter(B24478 >=-4 & B24025>=-4)
+lm.24478.24025<-lm(B24478~B24025, data=HostRange24.EOP.globi.filtered)
 summary(lm.24478.24025)
 # n.s.
 
@@ -138,8 +150,8 @@ HostRange24.EOP.globi<-HostRange24.EOP.globi %>%
 #view(HostRange24.EOP.globi)
 
 # medians?
-median(HostRange24.EOP.globi$B2880[HostRange24.EOP.globi$B2880 > -5]) #-1.255
-median(HostRange24.EOP.globi$B24025[HostRange24.EOP.globi$B24025>-5]) #-0.533
+median(HostRange24.EOP.globi$B2880[HostRange24.EOP.globi$B2880 > -5]) #-1.301
+median(HostRange24.EOP.globi$B24025[HostRange24.EOP.globi$B24025>-5]) #-0.301
 
 #plot globi v globi
 pHostRange24.EOP.globi<-HostRange24.EOP.globi %>%
@@ -203,7 +215,7 @@ pHostRange24.EOP.globi.oryzae2
 # how many cases (fraction of phage) have plaque formation on each host?
 HostRange24.phagesummary.all<-HostRange24.all %>%
   dplyr::filter(StrainLabel != "TARGET" & Set==1) %>%
-  group_by(Host, HostSpecies, StrainID) %>%
+  group_by(Host, HostSpecies, Bacteria) %>%
   summarize(nPhage=n(),
             countAny=sum(!is.na(EOP)),
             countLFW=sum(LFW=="Y"),
@@ -212,8 +224,8 @@ HostRange24.phagesummary.all<-HostRange24.all %>%
             fPlaque=countPlaque/nPhage)
 
 pAnyPlaques_bar_all<-HostRange24.phagesummary.all %>%
-  dplyr::filter(StrainID != "B2884" & StrainID != "B1814" & StrainID != "B4425") %>%
-  ggplot(aes(x=StrainID, y=fAny, fill=factor(StrainID)))+
+  dplyr::filter(Bacteria != "B2884" & Bacteria != "B1814" & Bacteria != "B4425") %>%
+  ggplot(aes(x=Bacteria, y=fAny, fill=factor(Bacteria)))+
   geom_col()+
   ylim(0,1)+
   theme_bw()+
@@ -229,8 +241,8 @@ pAnyPlaques_bar_all
 
 # plot out for countable plaquing only
 pIndivPlaques_bar_all<-HostRange24.phagesummary.all %>%
-  dplyr::filter(StrainID != "B24479" & StrainID != "B2884" & StrainID != "B1814" & StrainID != "B4425") %>%
-  ggplot(aes(x=StrainID, y=fPlaque, fill=factor(StrainID)))+
+  dplyr::filter(Bacteria != "B24479" & Bacteria != "B2884" & Bacteria != "B1814" & Bacteria != "B4425") %>%
+  ggplot(aes(x=Bacteria, y=fPlaque, fill=factor(Bacteria)))+
   geom_col()+
   theme_bw()+
   ylim(0,1)+
@@ -246,10 +258,18 @@ pIndivPlaques_bar_all
 
 #~~~~~~~~~~~~~~~~~~~
 # composite plot
-pHostRange24.EOP.multipanel<-plot_grid(pHostRange24.EOP.globi, pHostRange24.EOP.globi.oryzae1, pHostRange24.EOP.globi.oryzae2,
-          ncol=1)
+#pHostRange24.EOP.multipanel<-plot_grid(pHostRange24.EOP.globi, pHostRange24.EOP.globi.oryzae1, pHostRange24.EOP.globi.oryzae2,
+#          ncol=1)
 
-(pAnyPlaques_bar_all | pEOPbyHost.all) / (pEOPbyPhage.all.hm | pHostRange24.EOP.multipanel)  + 
-  plot_annotation(tag_levels = 'A') #+
+#(pAnyPlaques_bar_all | pEOPbyHost.all) / (pEOPbyPhage.filtered.hm | pHostRange24.EOP.multipanel)  + 
+#  plot_annotation(tag_levels = 'A') #+
 #  plot_layout(guides = 'collect')
-ggsave("GlobiPhageHostRangeSummaryPlotTall.png", width=12, height=14, units="in", dpi=500)
+
+pHostRange24.EOP.multipanel<-plot_grid(pHostRange24.EOP.globi, pHostRange24.EOP.globi.oryzae1, pHostRange24.EOP.globi.oryzae2,
+                                          nrow=1)
+                                       
+(pAnyPlaques_bar_all |  pEOPbyHost.all) / (pEOPbyPhage.filtered.hm / pHostRange24.EOP.multipanel)  + 
+  plot_annotation(tag_levels = 'A') +
+  plot_layout(heights=c(1,4))
+
+ggsave("GlobiPhageHostRangeSummaryPlotTall.png", width=14, height=14, units="in", dpi=500)
